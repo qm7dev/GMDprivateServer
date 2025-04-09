4c7ac7e85b876e290aeb3790e105f0a37d6f44fa
km7dev <gavin@t8er.com>
2025-04-09 16:06:18 -0400

    GDPS Switcher Integration (unfinished)

---
+++ config/gdpsSwitcher.php
@@ -0,0 +1,7 @@
+<?php
+# A message of the day for the server - Based off of Minecraft's system.
+# Color tags are supported, although fade, delay, and shake tags are not.
+# You can find all color tags at https://wyliemaster.github.io/gddocs/#/topics/tags?id=colour-tags
+# Alternatively, you can use hex colors, for example <c-AEAEAE>Text</c> would be gray.
+$motd = "A Geometry Dash Private Server";
+?>
